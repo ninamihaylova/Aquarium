9dd9c5788077992dc7897f580dbba35bc9a539f4
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-06-12 10:14:20 +0300

    Initial commit

---
+++ Aquarium/Program.cs
@@ -0,0 +1,15 @@
+﻿class Aquarium {
+    public static void Main (String[]args)
+    {
+
+        int lengthAquarium = int.Parse(Console.ReadLine());
+        int widthAquarium=int.Parse(Console.ReadLine());
+        int heightAquarium=int.Parse(Console.ReadLine());
+        double percent=double.Parse(Console.ReadLine());
+
+        double totalLitters=(lengthAquarium*widthAquarium*heightAquarium)*0.001;
+        double littersNeeded=totalLitters*(1-percent/100);
+        Console.WriteLine($"{littersNeeded:F2}");
+        
+    }
+}
